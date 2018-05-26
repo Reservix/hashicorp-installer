511c1af8cdf105e72c25278f4c5b8d70fdca922e
Michael Gerlach <michael.gerlach@reservix.de>
2018-05-26 15:51:56 +0200

    Unify option/parameter style

--- hashicorp-installer.sh
+++ hashicorp-installer.sh
@@ -8,7 +8,7 @@ install_prefix="${HOME}/bin"
 ver_lt() {
 	test "${1}" = "$(printf "%s\\n%s" "${1}" "${2}" \
 		| sort -V \
-		| head -n1)"
+		| head -n 1)"
 }
 
 # versions are equal
@@ -63,22 +63,22 @@ package_version_list=$(curl -s https://releases.hashicorp.com/index.json \
 # get latest version
 package_version=$(echo "${package_version_list}" \
 	| grep -E filename \
-	| awk -F'"' '{print $4}' \
-	| cut -d'_' -f2 \
+	| awk -F '"' '{print $4}' \
+	| cut -d '_' -f 2 \
 	| sort -V -r \
-	| head -1)
+	| head -n 1)
 
 # get package url of latest version
 package_url=$(echo "${package_version_list}" \
 	| grep -E "url.*${package_version}" \
-	| awk -F'"' '{print $4}')
+	| awk -F '"' '{print $4}')
 
 # check version difference
 command -v "${package}" > /dev/null 2>&1 \
 	&& {
 		package_version_installed=$("${package}" --version \
 			| head -n 1 \
-			| cut -d' ' -f2 \
+			| cut -d ' ' -f 2 \
 			| sed "s/v//")
 
 		ver_eq "${package_version_installed}" "${package_version}" \
@@ -104,7 +104,7 @@ curl -s -o "${package}.zip" "${package_url}"
 unzip -q "${package}.zip" -d "${package_install_path}"
 
 # set symlink
-ln -snf "${package_install_path}/${package}" "${HOME}/bin/${package}"
+ln -s -n -f "${package_install_path}/${package}" "${HOME}/bin/${package}"
 
 # exit gracefully
 echo "Successfully installed ${package}-${package_version}"
